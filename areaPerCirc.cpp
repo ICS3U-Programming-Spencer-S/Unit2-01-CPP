c9309c07c8328dc3712ea5c142ecdb0516cc03b2
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2022-09-22 21:39:59 +0000

    This program calculates the area and preimeter of a circle with a radius of 15mm

---
+++ areaPerCirc.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2021 Spencer Scarlett All rights reserved.
+//
+// Created by: Spencer Scarlett
+// Created on: Sept 22, 2022
+// This program calculates and displays the area
+// perimeter of a circle with radius 15 mm.
+#include <cmath>
+#include <iostream>
+
+int main() {
+    std::cout << "A circle has a radius of 15mm:\n";
+    std::cout << "Area = " << M_PI * pow(15, 2) << " cm^2\n";
+    std::cout << "Perimeter = " << 2 * M_PI * 15 << " cm\n";
+}
