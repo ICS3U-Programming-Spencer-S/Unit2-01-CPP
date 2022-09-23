4569fd28fe8d8fc8a52286431ed9f87d97dd84e9
Spencer Scarlett <Spencer.Scarlett@stu.ocsb.ca>
2022-09-23 13:03:46 +0000

    Fixed AreaPerCirc.cpp

--- areaPerCirc.cpp
+++ areaPerCirc.cpp
@@ -4,11 +4,12 @@
 // Created on: Sept 22, 2022
 // This program calculates and displays the area
 // perimeter of a circle with radius 15 mm.
+
 #include <cmath>
 #include <iostream>
 
 int main() {
-    std::cout << "A circle has a radius of 15mm:\n";
-    std::cout << "Area = " << M_PI * pow(15, 2) << " cm^2\n";
-    std::cout << "Perimeter = " << 2 * M_PI * 15 << " cm\n";
+    std::cout << "A circle has a radius of 15mm:\n" << std::endl;
+    std::cout << "Area = " << M_PI * pow(15, 2) << " cm^2\n" << std::endl;
+    std::cout << "Perimeter = " << 2 * M_PI * 15 << " cm\n" << std::endl;
 }
